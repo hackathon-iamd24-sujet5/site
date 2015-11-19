03ec8c53c668d9e77a47cace12c3076db3674254
Kevin Dalleau <kevin.dalleau@telecomnancy.eu>
2015-11-19 20:13:55 +0100

    New type of jQuery requests

--- d3test2.php
+++ d3test2.php
@@ -3,18 +3,21 @@ $data_recues = array (); // vient de spark, probablement quatre colonnes : état
 
 $data_color = array (); // va être donné à l'html, quatre colonnes aussi
 
-echo $_POST['envoi'];
+$month = $_POST['month'];
+$year = $_POST['year'];
+$data1 = $_POST['data1'];
+$data2 = $_POST['data2'];
 
 function requete() {
-	
+
 	$table = "meteo";
 	$db;
 	$sql;
 	$sql2;
 	$data = array();
 	$data2 = array();
-	
-	
+
+
 
 	// on se connecte � MySQL
 
@@ -23,74 +26,74 @@ function requete() {
 	} catch ( Exeception $e ) {
 		die ( 'Erreur : ' . $e->getMessage () );
 	}
-	
+
 	if($asked == "Tmin"){
-		
+
 		// si ça marche pas, hardcode tout ça
-	
+
 		$table = "meteo";
 		mysql_select_db ( 'hackathon' , $db );      // selection de base
-	
+
 		$sql = "SELECT COLUMN_NAME FROM meteo WHERE COLUMN_NAME like 'day%'";
-		$req = $db->query($sql);	
+		$req = $db->query($sql);
 		$data = $req->fetch();
-		
+
 		// print data si possible pour debugger
-		
+
 		$sql2 = "SELECT State,"+$data+" FROM meteo WHERE Year="+$year+"element=Tmin";
 		$req2 = $db->query($sql2);
 		$data2 = $req->fetch();
-		
+
 		$moyenne = array_sum($data2) / count($data2);
-	
+
 	}else if($asked == "Tmax"){
-	
+
 		$table = "meteo";
 		mysql_select_db ( 'hackathon' , $db );
-		
+
 		$sql = "SELECT COLUMN_NAME FROM meteo WHERE COLUMN_NAME like 'day%'";
 		$req = $db->query($sql);
 		$data = $req->fetch();
-		
+
 		$sql2 = "SELECT State,"+$data+" FROM meteo WHERE Year="+$year+"element=Tmax";
 		$req2 = $db->query($sql2);
 		$data2 = $req->fetch();
-		
+
 		$moyenne = array_sum($data2) / count($data2);
-	
+
 	}else if($asked == "car"){
-	
+
 		$table = "acc";
 		mysql_select_db ( 'hackathon' , $db );
 		$sql = "SELECT State,Population FROM acc WHERE Year="+$year;
-		
+
 		$req = $db->query($sql);
 		$data = $req->fetch();
-	
+
 	}else if($asked == "suicide"){
-	
+
 		$table = "suicide";
 		mysql_select_db ( 'hackathon' , $db );
 		$sql = "SELECT State,Death FROM suicide WHERE Year="+$year;
-		
+
 		$req = $db->query($sql);
 		$data = $req->fetch();
-	
+
 	}else if($asked == "air"){
-	
+
 		$table = "particules";
 		mysql_select_db ( 'hackathon' , $db );
 		$sql = "SELECT State,AvgFineParticulateMatter FROM acc WHERE Year="+$year;
-		
+
 		$req = $db->query($sql);
 		$data = $req->fetch();
-	
+
 	}else if($asked == "natality"){
-	
+
 		$table = "natality";
 		mysql_select_db ( 'hackathon' , $db );
 		$sql = "SELECT State,Births FROM acc WHERE Year="+$year;
-		
+
 		$req = $db->query($sql);
 		$data = $req->fetch();
 	}
